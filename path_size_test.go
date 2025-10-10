d10ab7fe8ab9cc3f7fa3ecf203f3bd2751bd5b73
pigrew <kukushonok.kukuhka@hmail.com>
2025-10-10 19:19:00 +0300

    тестовая функция #4

--- path_size_test.go
+++ path_size_test.go
@@ -11,38 +11,38 @@ import(
 )
 
 func TestGetSize(t *testing.T) {
-	rtdin,wstdin,_ := os.Pipe()
-	oldStdin := os.Stdin 
-	os.Stdin = rtdin 
+	rtdin,wstdin,_ := os.Pipe() // пайпуем Stdin вход
+	oldStdin := os.Stdin // сохраняем оригинал
+	os.Stdin = rtdin // перенапрвляем ввод на наш Pipe
 
-	rstdount,wstdount,_ := os.Pipe()
-	oldStdout := os.Stdout
-	os.Stdout = wstdount
+	rstdount,wstdount,_ := os.Pipe() // пайпуем Stdout выход
+	oldStdout := os.Stdout // сохраняем оригинал
+	os.Stdout = wstdount // перенапрвляем вывод на наш Pipe
 
-	_, err := io.WriteString(wstdin,"testdata/test.txt\n")
+	_, err := io.WriteString(wstdin,"testdata/test.txt\n") // запись в Stdin
 	if err != nil {
 		panic(err)
 	}
-	err = wstdin.Close()
+	err = wstdin.Close() // закрываем чтобы fmt не ждал бесконечно
 	if err != nil {
 		panic(err)
 	}
 
-	code.GetSize()
-	err = wstdount.Close()
+	code.GetSize() // вызовы функции которая принимает захваченный Stdin
+	err = wstdount.Close() // закрываем чтобы завершить чтение вывода
 	if err != nil {
 		panic(err)
 	}
-	os.Stdout = oldStdout
+	os.Stdout = oldStdout // возвращаем в исходное состояние
 	os.Stdin  = oldStdin
 
-	var buf bytes.Buffer
-	_, err = io.Copy(&buf, rstdount)
+	var buf bytes.Buffer // буффер для чтения вывода
+	_, err = io.Copy(&buf, rstdount) // копируем ответ GetSize в stdout
 	if err != nil {
 		panic(err)
 	}
 
-    output := buf.String()
-	result := fmt.Sprintf("%vB %s \n", 55, "testdata/test.txt")
-	require.Equal(t,output,result)
+    output := buf.String() // байты -> строка
+	result := fmt.Sprintf("%vB %s \n", 55, "testdata/test.txt") // ождиаемый результат
+	require.Equal(t,output,result) // проверка через testify
 }
